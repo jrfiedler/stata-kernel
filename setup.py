14b49b5222c389c099e19ae031d260d639775290
James Fiedler <jrfiedler@gmail.com>
2015-09-20 20:20:02 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='Stata kernel',
+	version='0.1',
+	description='Stata kernel for IPython',
+	author='James Fiedler',
+	author_email='jrfiedler@gmail.com',
+	url='https://github.com/jrfiedler/stata-kernel',
+	packages=['stata_kernel'],
+)
